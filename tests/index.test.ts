7fe8e4923a66bdb6c45eb7219bf5fa1beec96b6a
Ceola <lucas.ceola@catolicasc.edu.br>
2024-10-16 21:57:00 -0300

    pull request

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -5,3 +5,10 @@ describe('numeroPorExtenso', () => {
         expect(ext(1)).toBe('zero');
     });
 });
+
+describe('numeroPorExtenso', () => {
+    test('deve retornar o número por extenso corretamente', () => {
+        expect(ext(10)).toBe('');
+        expect(ext(-1)).toBe('');
+    });
+});
